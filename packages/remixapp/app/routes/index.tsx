344e0df6ede462c34ae5c57c25b1c7049d2110ad
Victor Savkin <mail@vsavkin.com>
2022-04-21 13:06:48 -0400

    init commit

---
+++ packages/remixapp/app/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from 'components';
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <Header></Header>
+    </div>
+  );
+}
